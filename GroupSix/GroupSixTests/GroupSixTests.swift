ad8bd8a47059c4171e49c7b194ade2655004071e
pasindusandamalsenanayake <pasindusandamalsenanayake@gmail.com>
2025-02-18 18:05:47 +0530

    intro part

---
+++ GroupSix/GroupSixTests/GroupSixTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroupSixTests.swift
+//  GroupSixTests
+//
+//  Created by Sandamal 014 on 2025-02-18.
+//
+
+import Testing
+@testable import GroupSix
+
+struct GroupSixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
